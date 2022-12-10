3a0d867d7d959fba0bf22615dcb99416e0bd0eac
Lewis Benson <tm.lewis.benson@gmail.com>
2022-12-10 15:53:31 -0500

    change to permissions for all models/routes

--- src/models/users.js
+++ src/models/users.js
@@ -10,7 +10,7 @@ const userModel = (sequelize, DataTypes) => {
     username: { type: DataTypes.STRING, required: true, unique: true },
     password: { type: DataTypes.STRING, required: true },
     vehicleType: { type: DataTypes.ENUM('car', 'truck', 'both'), required: true },
-    role: { type: DataTypes.ENUM('user', 'writer', 'editor', 'admin'), required: true, defaultValue: 'user' },
+    role: { type: DataTypes.ENUM('guest', 'writer', 'user', 'admin'), required: true, defaultValue: 'user' },
     token: {
       type: DataTypes.VIRTUAL,
       get() {
@@ -25,9 +25,9 @@ const userModel = (sequelize, DataTypes) => {
       type: DataTypes.VIRTUAL,
       get() {
         const acl = {
-          user: ['read'],
+          guest: ['read'],
           writer: ['read', 'create'],
-          editor: ['read', 'create', 'update'],
+          user: ['read', 'create', 'update'],
           admin: ['read', 'create', 'update', 'delete'],
         };
         return acl[this.role];
